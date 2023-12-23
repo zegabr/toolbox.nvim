e4652c9438127ca59e99188d679a8bb82ab0c9a1
zegabr <jjgabrielsp@gmail.com>
2023-12-23 01:00:45 -0300

    fix command definition
    Signed-off-by: zegabr <jjgabrielsp@gmail.com>

--- plugin/toolbox.lua
+++ plugin/toolbox.lua
@@ -1,7 +1,7 @@
 local toolbox = require('toolbox')
-vim.cmd([[command! -nargs=1 CdoMacro lua toolbox.cdo_macro(<f-args>)]])
-vim.cmd([[command! -nargs=1 CfdoMacro lua toolbox.cfdo_macro(<f-args>)]])
-vim.cmd([[command! -range -nargs=1 MultiLineMacro lua toolbox.multi_line_macro(<f-args>)]])
-vim.cmd([[command! CopyFilename lua toolbox.copy_filename()]])
-vim.cmd([[command! -range SearchReplaceSnippetRange lua toolbox.search_replace_snippet_on_range()]])
+vim.cmd([[command! -nargs=1 CdoMacro lua require('toolbox').cdo_macro(<f-args>)]])
+vim.cmd([[command! -nargs=1 CfdoMacro lua require('toolbox').cfdo_macro(<f-args>)]])
+vim.cmd([[command! -range -nargs=1 MultiLineMacro lua require('toolbox').multi_line_macro(<f-args>)]])
+vim.cmd([[command! CopyFilename lua require('toolbox').copy_filename()]])
+vim.cmd([[command! -range SearchReplaceSnippetRange lua require('toolbox').search_replace_snippet_on_range()]])
 return toolbox
