91013486f8e7f1a5557dd1f17c718fbad781ec2a
zegabr <jjgabrielsp@gmail.com>
2023-12-22 23:11:42 -0300

    update naming
    Signed-off-by: zegabr <jjgabrielsp@gmail.com>

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 Handful abstractions for intermediate/advanced vim functionalities
 
 # How to install
+You can install using your favorite plugin manager, as usual. Here is the way to install with lazy.nvim:
+```lua
+    {
+        'zegabr/toolbox.nvim',
+        config = function() -- (optional) this will run after loading the plugin
+            vim.keymap.set('n', '<leader><leader>%', require('toolbox').copy_filename,
+                { noremap = true, silent = true, desc = 'copy current file name to clipboard' })
+            vim.keymap.set("x", "<leader>m", ":MultiLineMacro ", { desc = 'run macro per line selected' })
+        end
+    },
+```
+
 # Exposed vim functions
+- CdoMacro | require('toolbox').cdo_macro
+- CfdoMacro | require('toolbox').cfdo_macro
+- MultiLineMacro | require('toolbox').multi_line_macro
+- CopyFilename | require('toolbox').copy_filename
--- lua/toolbox/init.lua
+++ lua/toolbox/init.lua
@@ -1,6 +1,6 @@
 return {
     -- Runs defined macro into all entries of quickfixlist
-    CdoMacro = function(char)
+    cdo_macro = function(char)
         -- Build the command string
         local command = string.format(':cdo execute "norm @%s" | update', char)
         -- Execute the command in Neovim
@@ -8,7 +8,7 @@ return {
     end,
 
     -- Runs defined macro into all files of quickfixlist
-    CfdoMacro = function(char)
+    cfdo_macro = function(char)
         -- Build the command string
         local command = string.format(':cfdo execute "norm @%s" | update', char)
         -- Execute the command in Neovim
@@ -16,14 +16,14 @@ return {
     end,
 
     -- Runs defined macro into all selected lines
-    MultiLineMacro = function(char)
+    multi_line_macro = function(char)
         -- Build the command string
         local command = string.format("'<,'>:norm @%s", char)
         -- Execute the command in Neovim
         vim.cmd(command)
     end,
 
-    CopyFilename = function()
+    copy_filename = function()
         vim.cmd([[:let @+ = fnamemodify(resolve(expand('%:p')), ':~:.')]])
     end,
 }
