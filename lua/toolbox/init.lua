9570926306b703c458b58dd74510afb7289f630d
zegabr <jjgabrielsp@gmail.com>
2023-12-22 22:48:40 -0300

    add CopyFilename
    Signed-off-by: zegabr <jjgabrielsp@gmail.com>

--- lua/toolbox/init.lua
+++ lua/toolbox/init.lua
@@ -1,24 +1,29 @@
--- Runs defined macro into all entries of quickfixlist
-function CdoMacro(char)
-    -- Build the command string
-    local command = string.format(':cdo execute "norm @%s" | update', char)
-    -- Execute the command in Neovim
-    vim.cmd(command)
-end
+return {
+    -- Runs defined macro into all entries of quickfixlist
+    CdoMacro = function(char)
+        -- Build the command string
+        local command = string.format(':cdo execute "norm @%s" | update', char)
+        -- Execute the command in Neovim
+        vim.cmd(command)
+    end,
 
--- Runs defined macro into all files of quickfixlist
-function CfdoMacro(char)
-    -- Build the command string
-    local command = string.format(':cfdo execute "norm @%s" | update', char)
-    -- Execute the command in Neovim
-    vim.cmd(command)
-end
+    -- Runs defined macro into all files of quickfixlist
+    CfdoMacro = function(char)
+        -- Build the command string
+        local command = string.format(':cfdo execute "norm @%s" | update', char)
+        -- Execute the command in Neovim
+        vim.cmd(command)
+    end,
 
--- Runs defined macro into all selected lines
-function MultiLineMacro(char)
-    -- Build the command string
-    local command = string.format("'<,'>:norm @%s", char)
-    -- Execute the command in Neovim
-    vim.cmd(command)
-end
+    -- Runs defined macro into all selected lines
+    MultiLineMacro = function(char)
+        -- Build the command string
+        local command = string.format("'<,'>:norm @%s", char)
+        -- Execute the command in Neovim
+        vim.cmd(command)
+    end,
 
+    CopyFilename = function()
+        vim.cmd([[:let @+ = fnamemodify(resolve(expand('%:p')), ':~:.')]])
+    end,
+}
