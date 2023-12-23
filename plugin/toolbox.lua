a8d9b77ff104314c28a18558f612184bfd4994f1
zegabr <jjgabrielsp@gmail.com>
2023-12-23 00:56:36 -0300

    fix requiring toolbox
    Signed-off-by: zegabr <jjgabrielsp@gmail.com>

--- plugin/toolbox.lua
+++ plugin/toolbox.lua
@@ -4,3 +4,4 @@ vim.cmd([[command! -nargs=1 CfdoMacro lua toolbox.cfdo_macro(<f-args>)]])
 vim.cmd([[command! -range -nargs=1 MultiLineMacro lua toolbox.multi_line_macro(<f-args>)]])
 vim.cmd([[command! CopyFilename lua toolbox.copy_filename()]])
 vim.cmd([[command! -range SearchReplaceSnippetRange lua toolbox.search_replace_snippet_on_range()]])
+return toolbox
